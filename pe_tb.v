1e2fdaa1b1a57e5b992a3e36fca2fc55b6e83d77
Hitesh Kumar MP <92785385+CyberKnight25@users.noreply.github.com>
2026-03-10 14:54:05 +0530

    Add files via upload

--- pe_tb.v
+++ pe_tb.v
@@ -12,6 +12,8 @@ wire [15:0] c00,c01,c02,c03;
 wire [15:0] c10,c11,c12,c13;
 wire [15:0] c20,c21,c22,c23;
 wire [15:0] c30,c31,c32,c33;
+wire [31:0] global_skips;
+wire [31:0] global_reuses;
 
 
 // DUT
@@ -22,7 +24,9 @@ b0,b1,b2,b3,
 c00,c01,c02,c03,
 c10,c11,c12,c13,
 c20,c21,c22,c23,
-c30,c31,c32,c33
+c30,c31,c32,c33,
+global_skips,    // NEW
+global_reuses
 );
 
 
@@ -56,8 +60,6 @@ reset=0;
 // 2 1 1 3
 // 0 2 3 1
 
-
-// feed diagonally
 @(posedge clk)
 a0=1; a1=0; a2=0; a3=0;
 b0=1; b1=0; b2=0; b3=0;
@@ -76,11 +78,11 @@ b0=0; b1=1; b2=2; b3=1;
 
 @(posedge clk)
 a0=0; a1=8; a2=3; a3=2;
-b0=0; b1=0; b2=1; b3=0;
+b0=0; b1=2; b2=1; b3=3;
 
 @(posedge clk)
 a0=0; a1=0; a2=2; a3=1;
-b0=0; b1=0; b2=0; b3=3;
+b0=0; b1=0; b2=3; b3=1;
 
 @(posedge clk)
 a0=0; a1=0; a2=0; a3=3;
@@ -89,6 +91,17 @@ b0=0; b1=0; b2=0; b3=1;
 
 // flush pipeline
 repeat(20) @(posedge clk);
+$display("A Matrix:");
+$display("%d %d %d %d", 1, 2, 3, 4);
+$display("%d %d %d %d", 5, 6, 7, 8);
+$display("%d %d %d %d", 2, 1, 3, 2);
+$display("%d %d %d %d", 4, 2, 1, 3);
+
+$display("B Matrix:");
+$display("%d %d %d %d", 1, 2, 0, 1);
+$display("%d %d %d %d", 3, 1, 2, 0);
+$display("%d %d %d %d", 2, 1, 1, 3);
+$display("%d %d %d %d", 0, 2, 3, 1);
 
 
 $display("Result Matrix:");
@@ -96,6 +109,13 @@ $display("%d %d %d %d",c00,c01,c02,c03);
 $display("%d %d %d %d",c10,c11,c12,c13);
 $display("%d %d %d %d",c20,c21,c22,c23);
 $display("%d %d %d %d",c30,c31,c32,c33);
+
+$display("-----------------------------------------");
+$display("POWER EFFICIENCY REPORT");
+$display("-----------------------------------------");
+$display("Total Zero-Skips (Clock Gated) : %0d", global_skips);
+$display("Total Temporal Reuses          : %0d", global_reuses);
+$display("-----------------------------------------");
 
 $finish;
 
